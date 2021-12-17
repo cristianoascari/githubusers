5c22c69f6d52de89b8c04f1cf38ad1b59450e60a
Cristiano Ascari <cristiano.ascari@gmail.com>
2021-12-17 20:19:50 -0300

    Main module configs.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,30 @@
 // Angular modules.
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+
+// Angular material.
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// Third-party.
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 // App routing.
 import { AppRoutingModule } from './app-routing.module';
 
 // App components.
 import { AppComponent } from './app.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) { return new TranslateHttpLoader(http); }
 
 @NgModule({
   declarations: [
@@ -16,10 +33,28 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    HttpClientModule,
+
+    MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+
+    TranslateModule.forRoot({
+      defaultLanguage: 'pt-BR',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
 
     AppRoutingModule,
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
+  githubapi: {
+    search: 'https://api.github.com/search/users',
+    users: 'https://api.github.com/users'
+  },
   production: true
 };
