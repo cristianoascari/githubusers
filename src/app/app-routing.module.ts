3df30a6d77e7550571dccfbf9ab55919b500caa8
Cristiano Ascari <cristiano.ascari@gmail.com>
2021-12-17 20:54:57 -0300

    New Users, Profile and Not-Found modules and basic routes config.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
-// Angular modules.
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from 'src/app/not-found/not-found.component';
+import { ProfileComponent } from 'src/app/profile/profile.component';
+import { UsersComponent } from 'src/app/users/users.component';
+
+const routes: Routes = [
+  { path: '', component: UsersComponent},
+  { path: 'user/:id', component: ProfileComponent},
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
